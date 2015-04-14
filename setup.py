409ce478e4665eb14d463763d418688205ed55b7
Jakub Jarosz <qba73@postpro.net>
2015-04-14 23:27:47 +0100

    Remove py3 supprt

--- setup.py
+++ setup.py
@@ -48,8 +48,5 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
     ],
 )
